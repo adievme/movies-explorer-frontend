c2057f814bcc77be8755e2935cf587e3b2f924be
Insaf Adiev <adievme99@yandex.ru>
2022-02-03 13:37:52 +0300

    refactor: изменить названия классов у блока about-project

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -3,25 +3,25 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <div className='about-project'>
-      <h1 className='about-project__header'>О проекте</h1>
-      <div className='about-project__container'>
-        <div className='about-project__text-container'>
-          <h2 className='about-project__header-text'>Дипломный проект включал 5 этапов</h2>
-          <p className='about-project__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+    <section className='about-project'>
+      <h2 className='about-project__header'>О проекте</h2>
+      <div className='about-project__description'>
+        <div className='about-project__columns'>
+          <h3 className='about-project__description-title'>Дипломный проект включал 5 этапов</h3>
+          <p className='about-project__description-subtitle'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </div>
-        <div className='about-project__text-container'>
-          <h2 className='about-project__header-text'>На выполнение диплома ушло 5 недель</h2>
-          <p className='about-project__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <div className='about-project__columns'>
+          <h3 className='about-project__description-title'>На выполнение диплома ушло 5 недель</h3>
+          <p className='about-project__description-subtitle'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </div>
       </div>
-      <div className='about-project__duration-container'>
-        <div className='about-project__duration about-project__duration_one'>1 неделя</div>
-        <div className='about-project__duration about-project__duration_two'>4 недели</div>
-        <p className='about-project__duration-caption'>Back-end</p>
-        <p className='about-project__duration-caption about-project__duration-caption_two'>Front-end</p>
+      <div className='about-project__weeks'>
+        <div className='about-project__weeks-item about-project__weeks-item_one'>1 неделя</div>
+        <div className='about-project__weeks-item about-project__weeks-item_two'>4 недели</div>
+        <p className='about-project__weeks-caption'>Back-end</p>
+        <p className='about-project__weeks-caption about-project__weeks-caption_two'>Front-end</p>
       </div>
-    </div>
+    </section>
   );
 }
 
